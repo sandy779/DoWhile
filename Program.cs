15240df83fc202f309693c754209fce1b8b78cdb
sandip ahire <sandipahire34@gmail.com>
2021-09-06 20:32:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, fact = 1, n;
+            Console.WriteLine("Enter limit");
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                fact = fact * i;
+                Console.WriteLine("Factorial of {0} is {1}", i, fact);
+                i++;
+                
+            } while (i <= n);
+        }
+    }
+}
